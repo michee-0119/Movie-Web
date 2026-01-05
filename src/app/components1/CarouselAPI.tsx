7f37ab8fe317cfd85eec836522feed7ddc3173c9
25LP2040 25LP2040 <michibaysaa@gmail.com>
2026-01-05 12:17:42 +0800

    trailer dutuu

--- src/app/components1/CarouselAPI.tsx
+++ src/app/components1/CarouselAPI.tsx
@@ -1,3 +1,5 @@
+import { fetchExternalImage } from "next/dist/server/image-optimizer";
+
 export async function CarouselAPI() {
   const response = await fetch(
     " https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
@@ -11,4 +13,38 @@ export async function CarouselAPI() {
 
   const data = await response.json();
   return data.results;
+}
+
+export async function seeMovieTrailer(movieId: number) {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_MOVIE_KEY}`,
+      },
+    }
+  );
+
+  const data = await res.json();
+
+  const trailer = data.results?.find(
+    (v: any) => v.type === "Trailer" && v.site === "Youtube"
+  );
+
+  return trailer?.key || null;
+}
+
+export async function seeMovieDetail(movieId: string) {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/movie/${movieId}?language=en-US`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_MOVIE_KEY}`,
+      },
+      cache: "no-store",
+    }
+  );
+
+  console.log("Movie API response status:", res.status);
+  if (!res.ok) return res.json();
 }
--- src/app/components1/PlayCarousel.tsx
+++ src/app/components1/PlayCarousel.tsx
@@ -2,6 +2,7 @@
 
 import * as React from "react";
 import Autoplay from "embla-carousel-autoplay";
+import { seeMovieTrailer } from "./CarouselAPI";
 
 import {
   Carousel,
@@ -18,58 +19,92 @@ type Movie = {
   title: string;
   backdrop_path: string;
   overview: string;
-  voteAverage: number;
+  vote_average: number;
 };
 
-export function PlayCarousel({ movies }: { movies: Movie }) {
+export function PlayCarousel({ movies }: { movies: Movie[] }) {
   const autoplay = React.useRef(
-    Autoplay({ delay: 2000, stopOnInteraction: true })
+    Autoplay({ delay: 3000, stopOnInteraction: true })
   );
 
+  const [trailerKey, setTrailerKey] = React.useState<string | null>(null);
+  const [open, setOpen] = React.useState(false);
+
+  const handleTrailer = async (movieId: number) => {
+    const key = await seeMovieTrailer(movieId);
+    setTrailerKey(key);
+    setOpen(true);
+  };
+
   return (
-    <Carousel
-      plugins={[autoplay.current]}
-      className="w-full h-[600px]"
-      onMouseEnter={autoplay.current.stop}
-      onMouseLeave={autoplay.current.reset}
-      opts={{ loop: true }}
-    >
-      <CarouselContent>
-        {movies.map((movie) => (
-          <CarouselItem key={movie.id}>
-            <img
-              src={`${imageBase}${movie.backdrop_path}`}
-              alt={movie.title}
-              className="w-full h-[600px]"
+    <>
+      <Carousel
+        plugins={[autoplay.current]}
+        className="w-full h-[600px]"
+        onMouseEnter={autoplay.current.stop}
+        onMouseLeave={autoplay.current.reset}
+        opts={{ loop: true }}
+      >
+        <CarouselContent>
+          {movies.map((movie) => (
+            <CarouselItem key={movie.id}>
+              <img
+                src={`${imageBase}${movie.backdrop_path}`}
+                alt={movie.title}
+                className="w-full h-[600px]"
+              />
+              <div className="absolute bottom-24 text-white flex flex-col h-[264px] mb-20 ml-[140px]">
+                <p className="text-[#FFFFFF] text-[16px] font-normal">
+                  Now Playing:
+                </p>
+
+                <h1 className="text-[36px] font-bold w-[410px]">
+                  {movie.title}
+                </h1>
+
+                <div className="flex text-{#FFFFF} text-[18px] mb-4 items-center">
+                  ⭐ {movie.vote_average.toFixed(1)}
+                  <p className="text-[#71717A] text-[16px]"> /10</p>
+                </div>
+
+                <p className="text-[#FAFAFA] text-[12px] w-[302px] pb-[16px]">
+                  {movie.overview}
+                </p>
+
+                <button
+                  onClick={() => handleTrailer(movie.id)}
+                  className="w-[145px] bg-white text-black flex items-center justify-center gap-[11.33px] font-medium rounded-md py-[5px] cursor-pointer"
+                >
+                  <img
+                    className="w-[9.33px] h-[12px]"
+                    src="/Vector (3).png"
+                    alt=""
+                  />
+                  <h6 className="text-[14px] font-normal">Watch Trailer</h6>
+                </button>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="ml-[100px]" />
+        <CarouselNext className="mr-[100px]" />
+      </Carousel>
+
+      {open && trailerKey && (
+        <div className="fixed inset-0 bg-black/80 z-50 flex items-center justify-center">
+          <div className=" relative w-[80%] max-w-4xl">
+            <button
+              onClick={() => setOpen(false)}
+              className="absolute -top-10 right-0 text-white text-xl"
+            ></button>
+            <iframe
+              className="w-full aspect-video rounded-lg"
+              src={`https://www.youtube.com/embed/${trailerKey}`}
+              allowFullScreen
             />
-            <div className="absolute bottom-24 text-white flex flex-col h-[264px] mb-20 ml-[140px]">
-              <p className="text-[#FFFFFF] text-[16px] font-normal">
-                Now Playing:
-              </p>
-              <h1 className="text-[36px] font-bold w-[410px]">{movie.title}</h1>
-
-              <p className="flex text-{#FFFFF} text-[18px] mb-4">
-                {/* ⭐ {movie.voteAverage.toFixed(1)} /10 */}
-                <p className="text-[#71717A] text-[16px]"></p>
-              </p>
-
-              <p className="text-[#FAFAFA] text-[12px] w-[302px] pb-[16px]">
-                {movie.overview}
-              </p>
-              <button className="w-[145px] bg-white text-black flex items-center justify-center gap-[11.33px] font-medium rounded-md py-[5px] cursor-pointer">
-                <img
-                  className="w-[9.33px] h-[12px]"
-                  src="play-button.png"
-                  alt=""
-                />
-                <h6 className="text-[14px] font-normal">Watch Trailer</h6>
-              </button>
-            </div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious className="ml-[100px]" />
-      <CarouselNext className="mr-[100px]" />
-    </Carousel>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
